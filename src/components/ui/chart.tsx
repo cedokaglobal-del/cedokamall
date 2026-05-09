601aff28ec46338bdf0356374816bd1eac8c44c8
Decodams <samueldim03@gmail.com>
2026-05-09 18:38:01 +0100

    remove style

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -65,26 +65,7 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
     return null;
   }
 
-  return (
-    <style
-      dangerouslySetInnerHTML={{
-        __html: (Object.entries(THEMES)
-          .map(
-            ([theme, prefix]) => `
-${prefix} [data-chart=${id}] {
-${colorConfig
-  .map(([key, itemConfig]) => {
-    const color = itemConfig.theme?.[theme as keyof typeof itemConfig.theme] || itemConfig.color;
-    return color ? `  --color-${key}: ${color};` : null;
-  })
-  .join("\n")}
-}
-`,
-          )
-          .join("\n") || ""),
-      }}
-    />
-  );
+  return <></>
 };
 
 const ChartTooltip = RechartsPrimitive.Tooltip;
